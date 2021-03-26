655c96d578ce448716ed56d2ad0e562ee0438f40
Urban <urban.etter@srf.ch>
2021-03-26 08:31:57 +0100

    Renamed add route to rate, some cleanup

--- src/ArticleService.php
+++ src/ArticleService.php
@@ -4,19 +4,30 @@
 namespace App;
 
 
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ArticleService
 {
-    private $httpClient;
+    private HttpClientInterface $httpClient;
+    private ParameterBagInterface $parameterBag;
 
-    public function __construct(HttpClientInterface $httpClient)
+    public function __construct(HttpClientInterface $httpClient, ParameterBagInterface $parameterBag)
     {
         $this->httpClient = $httpClient;
+        $this->parameterBag = $parameterBag;
     }
 
     public function info(int $id): array
     {
         return $this->httpClient->request('get', 'https://www.srf.ch/articleinfo/' . $id)->toArray();
+    }
+
+    public function seed(int $count = 10): array
+    {
+        $ids = $this->parameterBag->get('ids');
+        shuffle($ids);
+        $ids = array_slice($ids, 0, $count);
+        return array_map(fn($id) => ['id' => $id, 'certainty' => 1], $ids);
     }
 }
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\ArticleService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -13,99 +14,37 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(ArticleService $articleService): Response
     {
-        // $handle = fopen("../data/mini-articles.csv", "r");
-        // $isFirst = true;
-        // while ($line = fgetcsv($handle, 0, ";")) {
-        //     if (!$isFirst) {
-
-        //         var_dump($line[3]); die;
-
-        //     }
-        //     $isFirst = false;
-        // };
-        // fclose($handle);
-
-
-       return $this->render('base.html.twig', ["engine" => "stuff"]);
+        $seed = [
+            'name' => 'Random article rater',
+            'certainty' => 1,
+            'reason' => 'To get the initial set of articles to rate',
+            'articles' => $articleService->seed(10)
+        ];
+        return $this->render('base.html.twig', ['recommender' => $seed]);
     }
 
     /**
-     * @Route("/add/{id}/{rating}", name="add")
+     * @Route("/rate/{id}/{rating}", name="rate")
      */
-    public function addArticle(int $id, int $rating, SessionInterface $session)
+    public function rate(int $id, int $rating, SessionInterface $session)
     {
         $articles = $session->get('articles', []);
         $articles[] = ['id' => $id, 'rating' => $rating];
         $session->set('articles', $articles);
 
-        return $this->json(['msg' => 'article recorded', 'count' => count($articles)]);
+        return $this->json(['msg' => 'article recorded', 'count' => count($articles), 'articles' => $articles]);
     }
 
     /**
      * @Route("/send", name="send")
      */
-    public function send(SessionInterface $session)
+    public function send(SessionInterface $session, HttpClientInterface $httpClient)
     {
-        $fakeResponse = [
-            'name' => 'author_recom',
-            'reason' => 'Aufgrund Ihrer Angaben, Popularität und Aktualität werden Ihnen die Artikel von [Platzhalter siehe Reason unten] angezeigt.',
-            'certainty' => 1,
-            'articles' => [
-                0 => [
-                    'id' => 19545671,
-                    'certainty' => 0.6290087797531265,
-                    'reason' => 'Peter Fritsche',
-                ],
-                1 => [
-                    'id' => 19548257,
-                    'certainty' => 0.6078855789610188,
-                    'reason' => 'Dario Pelosi',
-                ],
-                2 => [
-                    'id' => 19546865,
-                    'certainty' => 0.388069011564527,
-                    'reason' => 'Marielle Gygax',
-                ],
-                3 => [
-                    'id' => 19510364,
-                    'certainty' => 0.3545522240208885,
-                    'reason' => 'Dario Pelosi',
-                ],
-                4 => [
-                    'id' => 19553900,
-                    'certainty' => 0.32489756575980105,
-                    'reason' => 'Dario Pelosi',
-                ],
-                5 => [
-                    'id' => 19489481,
-                    'certainty' => 0.293741931031462,
-                    'reason' => 'Peter Fritsche',
-                ],
-                6 => [
-                    'id' => 19467500,
-                    'certainty' => 0.2746112166143708,
-                    'reason' => 'Benjamin Hostettler',
-                ],
-                7 => [
-                    'id' => 19465592,
-                    'certainty' => 0.2628699385891371,
-                    'reason' => 'Peter Fritsche',
-                ],
-                8 => [
-                    'id' => 19451755,
-                    'certainty' => 0.22968875061756835,
-                    'reason' => 'Peter Fritsche',
-                ],
-                9 => [
-                    'id' => 19452205,
-                    'certainty' => 0.22546642646038853,
-                    'reason' => 'David Karasek',
-                ],
-            ],
-        ];
+        $articles = $session->get('articles');
+        $response = $httpClient->request('GET', 'https://my-srf.herokuapp.com/recommender/author', ['json' => $articles]);
         $session->set('articles', []);
-        return $this->render('base.html.twig', ['recommender' => $fakeResponse]);
+        return $this->render('base.html.twig', ['recommender' => $response->toArray()]);
     }
 }
