5210ea0d61c7290e4099ea67c929881ae6ef3aeb
Urban <urban.etter@srf.ch>
2021-03-26 14:13:06 +0100

    Delete stuff we don't need

--- src/ArticleService.php
+++ src/ArticleService.php
@@ -4,31 +4,20 @@
 namespace App;
 
 
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ArticleService
 {
     private HttpClientInterface $httpClient;
-    private ParameterBagInterface $parameterBag;
 
-    public function __construct(HttpClientInterface $httpClient, ParameterBagInterface $parameterBag)
+    public function __construct(HttpClientInterface $httpClient)
     {
         $this->httpClient = $httpClient;
-        $this->parameterBag = $parameterBag;
     }
 
     public function info(int $id): array
     {
         return $this->httpClient->request('GET', 'https://www.srf.ch/articleinfo/' . $id)->toArray();
-    }
-
-    public function seed(int $count = 10): array
-    {
-        $ids = explode(",", $this->parameterBag->get('ids'));
-        shuffle($ids);
-        $ids = array_slice($ids, 0, $count);
-        return array_map(fn($id) => ['id' => $id, 'certainty' => 1, 'reason' => 'Random'], $ids);
     }
 
     public function enrichRecommenderInfos(array $articles): array
